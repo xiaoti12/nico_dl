b19b16642ad675f4a8257544959c7549cd950db4
xiaoti <xiaoti@ljy.com>
2024-02-02 01:33:04 +0800

    auto load file name

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	"github.com/go-resty/resty/v2"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var (
-	client  = resty.New()
-	cookies string
-)
-var (
-	CookieFile = "nico_cookie.txt"
-	MU38File   = "entering.m3u8"
+	client     = resty.New()
+	cookies    string
+	CookieFile string
+	MU38File   string
 )
 
 func main() {
+	loadFileNames()
 	loadCookie(CookieFile)
 	m3u8URLs := findM3U8(MU38File)
 	for i, url := range m3u8URLs {
@@ -47,4 +47,23 @@ func getM3U8(url string) []byte {
 		log.Fatalf("Error getting m3u8: %s\n", err)
 	}
 	return resp.Body()
+}
+
+func loadFileNames() {
+	CookieFile = findNameWithExt("txt")
+	MU38File = findNameWithExt("m3u8")
+}
+
+func findNameWithExt(ext string) string {
+	files, err := filepath.Glob("*." + ext)
+	if err != nil {
+		log.Fatalf("Error searching for %s file: %v\n", ext, err)
+	}
+	if len(files) == 0 {
+		log.Fatalf("No %s file found", ext)
+	}
+	if len(files) > 1 {
+		log.Fatalf("More than one %s file found", ext)
+	}
+	return files[0]
 }
